379242aca3ddb9f7e014767b4f0901fc73ca17f2
hernanimattos <hernani.luiz@gmail.com>
2023-06-20 18:44:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-output-file-paths
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-output-file-paths)
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          console.log(assetInfo, '-a-a-a-a-a-a-a-a-a-a');
+
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+      },
+    },
+  },
+});
